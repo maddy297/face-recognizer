d5f52f32fdc828726be9374f32ba6491a9ff6e3c
maddy297 <81658887+maddy297@users.noreply.github.com>
2021-10-05 16:25:08 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
-Webcam.set({
+      
+Webcam.attach( '#camera' );
+
+camera = document.getElementById("camera");
+      
+  Webcam.set({
     width:350,
     height:300,
-    image_format:'png',
+    image_format : 'png',
     png_quality:90
-});
-
-camera=document.getElementById("camera");
-
-Webcam.attach('#camera');
+  });
 
 function take_snapshot()
 {
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
-
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
     });
 }
 
-console.log('ml5 version:', ml5.version);
+  console.log('ml5 version:', ml5.version);
+  
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/kH92c6eqZ/model.json',modelLoaded);
 
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/kH92c6eqZ/model.json',modeLoaded);
+  
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+function gotResult(error, results) {
+  
+  if (error) {
+    console.error(error);
+  } else {
+    
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
